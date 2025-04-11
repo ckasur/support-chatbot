195a57305f1387205ce4423a9d3c061eeb2e4d87
Karine Suryatna <129222200+ckasur@users.noreply.github.com>
2025-04-11 15:01:40 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Install pyenv if missing
-if ! command -v pyenv &>/dev/null; then
+# Check if pyenv is installed, if not, install it
+if [ ! -d "$HOME/.pyenv" ]; then
     curl https://pyenv.run | bash
 fi
+
+# Add pyenv to the path
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 
 # Install the required Python version
 pyenv install 3.11.11
